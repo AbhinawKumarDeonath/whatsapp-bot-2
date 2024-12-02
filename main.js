57195ce3e0e5e73bd4614bf33ac96c08cad72100
AbhinawKumarDeonath <abhinawdeonath30@gmail.com>
2024-12-02 17:03:57 +0530

    added no auth

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, NoAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 // Create a new client instance
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: './session_data' // Specify folder to save session data
-    }),
+    authStrategy: new NoAuth(),
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     }
 });
+
+
+
+
 
 
 // When the client is ready, run this code (only once)
@@ -24,7 +26,7 @@ client.on('qr', (qr) => {
  
 });
 client.on('message_create', message => {
-	if (message.body === 'Hi') {
+	if (message.body === 'hi') {
 		// send back "pong" to the chat the message was sent in
 		client.sendMessage(message.from, 'Hello! How can I help you?');
 	}
